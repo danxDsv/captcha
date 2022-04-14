26e00e31e7164a8ff544231de2990068a92d19f3
danxDsv <dany-2001@hotmail.fr>
2022-04-14 16:15:37 +0200

    pattern

--- formulaire.php
+++ formulaire.php
@@ -16,19 +16,19 @@ blacklistFORM();
     <form action="traitement.php" method="post">
 		<!-- vrais input du formulaire mettre des patterns si possible pour bloquer spam d'url-->
 		<p>
-			<input type="text" name="nom" id="nom" pattern="[A-Za-z]*" placeholder="Nom"/>
+			<input type="text" name="nom" id="nom" pattern="^[a-zA-Z][a-zA-Z .,'-]*$" placeholder="Nom"/>
 		</p>
 		<p>
-			<input type="text" name="prenom" id="prenom" pattern="[A-Za-z]*" placeholder="Prenom"/>
+			<input type="text" name="prenom" id="prenom" pattern="^[a-zA-Z][a-zA-Z .,'-]*$" placeholder="Prenom"/>
 		</p>
 		<!--Pot de miel JS-->
         <p>
-			<input type="tel" name="telephone" id="telephone" tabindex="-1" autocomplete="off" placeholder="telephone"/>
+			<input type="tel" name="telephone" id="telephone" tabindex="-1" autocomplete="off" placeholder="telephone" value=""/>
 		</p>
 		<!--Pot de miel CSS-->
         <p>
 			<!--<input type="text" name="validation" tabindex="-1" autocomplete="off" id="validation" placeholder="validation"/>-->
-            <input type="text" name="validation" style="display:none !important" tabindex="-1" autocomplete="off" id="validation" placeholder="validation"/>
+            <input type="text" name="validation" style="display:none !important" tabindex="-1" autocomplete="off" id="validation" placeholder="validation" value=""/>
 		</p>	
 		<p>
 			<input type="submit" value="Suivant" name="send" />
