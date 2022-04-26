ebb5a8b53d8c5923fc3d722f5c24eb487ed4812f
danxDsv <dany-2001@hotmail.fr>
2022-04-26 16:15:26 +0200

    fix empty bug

--- traitement.php
+++ traitement.php
@@ -35,13 +35,13 @@ if (!$_SESSION["visite"]) {
 
 //verification submit
 if(isset($_POST["send"])){
-
+    
     //verification pot de miel CSS vide
     if(isset($_POST["validation"]) && empty($_POST["validation"])){
-
+        
         //verification pot de miel JS vide
         if (isset($_POST["telephone"]) && empty($_POST["telephone"])) {
-
+            
             //verification que les champs sont remplis
             if(
                 isset($_POST["nom"]) && !empty($_POST["nom"]) &&
@@ -49,8 +49,51 @@ if(isset($_POST["send"])){
             ){
                 $nom = strip_tags($_POST["nom"]);
                 $prenom = strip_tags($_POST["prenom"]);
-                //si pas de captcha fin traitement du form
-                if (!$BoolCaptcha) {
+
+                //si le captcha est présent
+                if ($BoolCaptcha) {
+                    //si le captcha a été envoyé et qu'il n'est pas vide
+                    if (isset($_POST["captcha"]) && is_numeric($_POST["captcha"])) {
+                        //si le résultat en chiffre et en lettre sont faux
+                        if ( ($_POST["captcha"] != $_SESSION["captcha"]) && ($_POST["captcha"] != $_SESSION["captchaLettre"]) ) {
+                            echo "<script type=\"text/javascript\">";
+                            echo "alert('Le résultat du calcul est faux');";
+                            echo "document.location.href='formulaire.php';";
+                            echo "</script>";
+                        } else {
+                            //supprimer resultat pour que le resultat puisse changer avec retour en arrière
+                            unset($_SESSION["captcha"]);
+                            unset($_SESSION["captchaLettre"]);
+                            $_SESSION["nbForm"] += 1;
+                            //limite le nombre de formulaire à 3 par jour
+                            if ($_SESSION["nbForm"] > 3) {
+                                //temps restant
+                                $tps = ($limiteTps)-(time()-$_SESSION['start']);
+                                $tpsMin = (int)($tps/60);
+                                $tpsH = (int)($tpsMin/60);
+                                
+                                //message d'erreur
+                                if ($tps < 60) {
+                                    echo "<script type=\"text/javascript\">";
+                                    echo "alert('Vous ne pouvez plus envoyer de formulaire pour le moment, Attendez '+'$tps'+' seconde(s)');";
+                                    echo "document.location.href='formulaire.php';";
+                                    echo "</script>";
+                                } elseif ($tpsH >= 1) {
+                                    echo "<script type=\"text/javascript\">";
+                                    echo "alert('Vous ne pouvez plus envoyer de formulaire pour le moment, Attendez '+'$tpsH'+' heure(s)');";
+                                    echo "document.location.href='formulaire.php';";
+                                    echo "</script>";
+                                } else {
+                                    echo "<script type=\"text/javascript\">";
+                                    echo "alert('Vous ne pouvez plus envoyer de formulaire pour le moment, Attendez '+'$tpsMin'+' minute(s)');";
+                                    echo "document.location.href='formulaire.php';";
+                                    echo "</script>";
+                                }           
+                            }
+                            header('location: réussi.php');
+                        }
+                    }
+                } else {
                     $_SESSION["nbForm"] += 1;
                     //limite le nombre de formulaire à 3 par jour
                     if ($_SESSION["nbForm"] > 3) {
@@ -82,53 +125,6 @@ if(isset($_POST["send"])){
             } else {
                 header('location: formulaire.php');
             }
-            
-            //si le captcha est présent
-            if ($BoolCaptcha) {
-                //si le captcha a été envoyé et qu'il n'est pas vide
-                if (isset($_POST["captcha"]) && !empty($_POST["captcha"])) {
-                    //si le résultat en chiffre et en lettre sont faux
-                    if ( ($_POST["captcha"] != $_SESSION["captcha"]) && ($_POST["captcha"] != $_SESSION["captchaLettre"]) ) {
-                        echo "<script type=\"text/javascript\">";
-                        echo "alert('Le résultat du calcul est faux');";
-                        echo "document.location.href='formulaire.php';";
-                        echo "</script>";
-                    } else {
-                        //supprimer resultat pour que le resultat puisse changer avec retour en arrière
-                        unset($_SESSION["captcha"]);
-                        unset($_SESSION["captchaLettre"]);
-                        $_SESSION["nbForm"] += 1;
-                        //limite le nombre de formulaire à 3 par jour
-                        if ($_SESSION["nbForm"] > 3) {
-                            //temps restant
-                            $tps = ($limiteTps)-(time()-$_SESSION['start']);
-                            $tpsMin = (int)($tps/60);
-                            $tpsH = (int)($tpsMin/60);
-                            
-                            //message d'erreur
-                            if ($tps < 60) {
-                                echo "<script type=\"text/javascript\">";
-                                echo "alert('Vous ne pouvez plus envoyer de formulaire pour le moment, Attendez '+'$tps'+' seconde(s)');";
-                                echo "document.location.href='formulaire.php';";
-                                echo "</script>";
-                            } elseif ($tpsH >= 1) {
-                                echo "<script type=\"text/javascript\">";
-                                echo "alert('Vous ne pouvez plus envoyer de formulaire pour le moment, Attendez '+'$tpsH'+' heure(s)');";
-                                echo "document.location.href='formulaire.php';";
-                                echo "</script>";
-                            } else {
-                                echo "<script type=\"text/javascript\">";
-                                echo "alert('Vous ne pouvez plus envoyer de formulaire pour le moment, Attendez '+'$tpsMin'+' minute(s)');";
-                                echo "document.location.href='formulaire.php';";
-                                echo "</script>";
-                            }           
-                        }
-                        header('location: réussi.php');
-                    }
-                }
-            } else {
-                header('location: réussi.php');
-            }
         }else{
             blacklist(); 
         }
@@ -140,6 +136,16 @@ if(isset($_POST["send"])){
     header('location: formulaire.php');
 
 }
-
-
 ?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Formulaire</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <script src="js/script.js"></script>
+</body>
+</html>
